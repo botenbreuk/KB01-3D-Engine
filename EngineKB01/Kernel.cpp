496d2d71eddd9f651f5c5d8e5f514c50400e751e
Kajaffie <Jafeth.vanElten@hotmail.com>
2015-01-06 14:06:43 +0100

    Support toegevoegd voor meerdere renderers, renderertype kan nu gekozen worden maar is voor het blok hard-coded.

--- EngineKB01/Kernel.cpp
+++ EngineKB01/Kernel.cpp
@@ -2,7 +2,8 @@
 
 Kernel::Kernel()
 {
-	
+	//Can later be refactored to ask/check which type of Renderer to use.
+	_usedType = RendererType::DirectX;
 }
 
 Kernel::~Kernel()
@@ -14,11 +15,21 @@ Initialises the compartments of the Engine.
 */
 void Kernel::Initialize()
 {
-
 	_logger = new Log();
-	_renderer = new DirectXRenderer(_logger);
+	switch (_usedType)
+	{
+		case DirectX:	_renderers[_usedType] = new DirectXRenderer(_logger);
+			_logger->WriteLog("Renderer used: DirectX", Log::MessageType::Info);
+			break;
+		case OpenGL:	//TODO: Implement.
+			_logger->WriteLog("Renderer used: OpenGL", Log::MessageType::Info);
+			break;
+		case Software:	//TODO: Implement.
+			_logger->WriteLog("Renderer used: Software", Log::MessageType::Info);
+			break;
+	}
 	_resourceManager = new ResourceManager();
-	_meshManager = new MeshManager(_logger,(DirectXRenderer*)_renderer);
+	_meshManager = new MeshManager(_logger,(DirectXRenderer*)_renderers[_usedType]);
 	_windowManager = new WindowManager();
 	_sceneManager = new SceneManager();
 
@@ -37,7 +48,7 @@ void Kernel::Run()
 	HWND hWND = _windowManager->CreateNewWindow();
 
 	//Initialises 3D
-	_renderer->Init3D(hWND);
+	_renderers[_usedType]->Init3D(hWND);
 
 	//Loads in the Meshes.
 	_meshManager->LoadMeshes();
@@ -64,8 +75,7 @@ void Kernel::Run()
 		else
 		{
 			//Renders all the Scenes of this Engine.
-			_sceneManager->RenderAllScenes(_renderer, _meshManager);
-	
+			_sceneManager->RenderAllScenes(_renderers[_usedType], _meshManager);
 		}
 	}
 }
@@ -75,7 +85,10 @@ Cleans up the compartments of the Engine.
 */
 void Kernel::CleanUp()
 {
-	delete _renderer;
+	for(int i = RendererType::First; i < RendererType::Last; i++)
+	{
+		_renderers.erase((RendererType)i);
+	}
 	delete _resourceManager;
 	delete _meshManager;
 	delete _windowManager;
